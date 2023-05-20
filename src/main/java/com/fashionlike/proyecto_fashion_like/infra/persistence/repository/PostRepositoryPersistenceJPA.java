cf5c94d7d71dd404d8cbb2e7062ac3772a6889c4
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-05-19 22:58:23 -0300

    Refactors

--- src/main/java/com/fashionlike/proyecto_fashion_like/infra/persistence/repository/PostRepositoryPersistenceJPA.java
+++ src/main/java/com/fashionlike/proyecto_fashion_like/infra/persistence/repository/PostRepositoryPersistenceJPA.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepositoryPersistenceJPA extends JpaRepository<PostEntity, Long> {
+public interface PostRepositoryPersistenceJPA extends JpaRepository<PostEntity, Integer> {
 }
